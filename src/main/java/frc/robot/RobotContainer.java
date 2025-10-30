13e762b252ab043d1077afbd01c4fda4d5032284
TrinityTritons <trinitytritons4215@gmail.com>
2025-10-30 16:25:05 -0500

    Inital commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,6 +25,7 @@ public class RobotContainer {
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
+      private double hell_yeah;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
